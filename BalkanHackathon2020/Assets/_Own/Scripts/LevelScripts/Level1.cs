767884fa69d2ad74050ce702d8134edc71640e4a
Igleer <milaqiigli@gmail.com>
2020-07-15 21:30:11 +0200

    Added turret laser.

--- BalkanHackathon2020/Assets/_Own/Scripts/LevelScripts/Level1.cs
+++ BalkanHackathon2020/Assets/_Own/Scripts/LevelScripts/Level1.cs
@@ -6,19 +6,27 @@ using UnityEngine;
 public class Level1 : AbstractLevel
 {
    public Transform[] turretHeads;
+   public Transform[] aimTransforms;
+   public LineRenderer[] turretAims;
    public Transform playerTransform;
+   public float turretReloadTime = 2f;
+   
    private Vector3 targetPos;
-
+   
    private void Update()
    {
       run = true;
       if (run)
       {
          targetPos = playerTransform.position;
-         foreach (Transform t in turretHeads)
+         for (int i = 0; i < 2; i++)
          {
-            t.LookAt(targetPos);
+            turretHeads[i].LookAt(targetPos);
+            turretAims[i].SetPosition(0,aimTransforms[i].position);
+            turretAims[i].SetPosition(1,targetPos);
          }
+         
       }
    }
+   
 }
